27ac3beacfd13a5421c42bd8e6355f2e2c12b5ec
Yannik Schröder <yannik.schroeder@gmail.com>
2019-09-09 11:12:05 +0200

    added pixi

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,55 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-require("@babel/register");
+require('@babel/register');
 
+const publicPath = path.resolve('/public');
 const config = {
-  entry: ['@babel/polyfill','./src/index.js'],
-  output: {
-    path: __dirname + '/public',
-    filename: 'bundle.js'
-  },
-  module: {
-    rules : [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-        hash: true
-    })
-  ],
-  resolve: {
-    modules: [
-      path.resolve('./src'),
-      path.resolve('./node_modules')
-    ]
-  },
-  devServer: {
-    contentBase: __dirname + '/public',
-    compress: true,
-    port: 9000,
-    open: true,
-    stats: {
-        assets: false,
-        children: false,
-        chunks: false,
-        chunkModules: false,
-        colors: true,
-        entrypoints: false,
-        hash: false,
-        modules: false,
-        timings: false,
-        version: false,
-    }
-  },
-  watch: false,
-  devtool: 'source-map',
+	entry: ['@babel/polyfill', './src/index.js'],
+	output: {
+		path: publicPath,
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: ['babel-loader']
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			hash: true
+		})
+	],
+	resolve: {
+		modules: [path.resolve('./src'), path.resolve('./node_modules')]
+	},
+	devServer: {
+		contentBase: publicPath,
+		compress: true,
+		port: 9000,
+		open: true,
+		stats: {
+			assets: false,
+			children: false,
+			chunks: false,
+			chunkModules: false,
+			colors: true,
+			entrypoints: false,
+			hash: false,
+			modules: false,
+			timings: false,
+			version: false
+		}
+	},
+	watch: false,
+	devtool: 'source-map'
 };
 
 module.exports = config;
